f093c53a41c956eaf47085dc73689c998cd46cf5
MohimSingla <Mohim.singla@skeps.com>
2023-07-27 14:44:37 +0530

    Cleaning codebase

--- routes/routes.js
+++ routes/routes.js
@@ -3,27 +3,12 @@ const { saveBlock, getBlockData, getChainSummary } = require('../controllers/blo
 
 const routes = new express.Router();
 
+
 routes.post('/block', saveBlock);
 
 routes.get('/block/:blockNumber', getBlockData);
 
 routes.get('/blocks/stats', getChainSummary);
+
+
 module.exports = { routes };
-
-// Create a NodeJS HTTP server with support for following APIs
-// i. POST /block
-// 1. To add a new block in the blockchain
-// 2. Request - { “data”: “123” }
-// 3. Response - { “blockNo”: “”, “blockHash”: “”, “nounce”: “”, “previousHash”: “” }
-
-
-// ii. GET /block
-// 1. To get an existing block from the chain
-// 2. Request query params: blockNo
-// 3. Response: { “blockHash”: “”, “data”: “”, “nounce”: “”, “previousHash”: “”, “blockNo”: “” }
-
-
-// iii. GET /block/stats
-// 1. To get info about the chain
-// 2. No request query params
-// 3. Response: { “blockCount”: “”, “latestBlockHash”: “” }
